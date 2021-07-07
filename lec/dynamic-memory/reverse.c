37594a126babeec5bd988d25f7c3d4a60b9abdca
James Zhang <jameszhanga@gmail.com>
2021-07-07 17:06:43 -0500

    function returns address of local variable error. need to return address outside of function scope

--- lec/dynamic-memory/reverse.c
+++ lec/dynamic-memory/reverse.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
-
 void print_array(int *a);
+int *reverse(int a[]);
 
 int main() {
-
-    int a[5] = {1,2,3,4,5};
+    int a[5] = {1, 2, 3, 4, 5};
     print_array(a);
+
+    int *p = reverse(a);
+    print_array(p);
 }
 
-void print_array(int *a){
-    for (int i=0;i<5;i++) 
-        printf("%d ",a[i]);
+void print_array(int *a) {
+    for (int i = 0; i < 5; i++)
+        printf("%d ", a[i]);
     printf("\n");
+}
+
+int *reverse(int a[]) {
+    int b[5];
+    for (int i = 0; i < 5; i++) {
+        b[i] = a[5 - i];
+    }
+    return b;
 }
