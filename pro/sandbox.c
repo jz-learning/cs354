0240dad8457a4b3dae9384463ef75a2e1dab46d8
James Zhang <jameszhanga@gmail.com>
2021-07-17 16:47:27 -0500

    Testing how assignment and comparing in the same line works

--- pro/sandbox.c
+++ pro/sandbox.c
@@ -6,8 +6,12 @@ int main() {
     char arr[6] = "hello";
 
     printf("%c\n", at(arr, 1));
+
+    char a = 0;
+    if (a = 'c')
+        printf("true\n");
+    else
+        printf("false\n");
 }
 
-char at(char *arr, int idx) {
-    return *(arr + idx * sizeof(char));
-}
+char at(char *arr, int idx) { return *(arr + idx * sizeof(char)); }
