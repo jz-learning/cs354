830fc938887c7d00d7fb339f01424094db2d3e29
James Zhang <jameszhanga@gmail.com>
2021-07-18 01:04:22 -0500

    Figure out how pointer arithmic works

--- pro/sandbox.c
+++ pro/sandbox.c
@@ -3,15 +3,26 @@
 char at(char *arr, int idx);
 
 int main() {
-    char arr[6] = "hello";
+    int arr[5] = {2, 4, 6, 8, 10};
 
-    printf("%c\n", at(arr, 1));
+    // printf("%c\n", at(arr, 1));
 
-    char a = 0;
-    if (a = 'c')
-        printf("true\n");
-    else
-        printf("false\n");
+    // char a = 0;
+    // if (a = 'c')
+    //     printf("true\n");
+    // else
+    //     printf("false\n");
+
+    for (int i = 0; i < 5; i++) {
+        printf("%i\n", arr[i]);
+        printf("Address of arr: %p\n", &arr[i]);
+    }
+    printf("=============\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%i \n", *(arr + i));
+        printf("Address of arr: %p\n", arr);
+        printf("Address of arr: %p\n", &*(arr + i));
+    }
 }
 
 char at(char *arr, int idx) { return *(arr + idx * sizeof(char)); }
---
+++ pro/tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+
+    // printf("%c\n", at(arr, 1));
+
+    // char a = 0;
+    // if (a = 'c')
+    //     printf("true\n");
+    // else
+    //     printf("false\n");
