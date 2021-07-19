9c02ab1a28905cb40d10c7583d9883b5c1129c8a
James Zhang <jameszhanga@gmail.com>
2021-07-18 20:44:36 -0500

    Add notes for midterm

---
+++ Exam/e1/problems.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int counter;
+
+    for (int i = 0; i < 10; i++) {
+        int counter = counter + 1;
+    }
+
+    printf("%d\n", counter);
+    return 0;
+}
